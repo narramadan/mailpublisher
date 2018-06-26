6723da4bc41df5b9a255208ab15cd443448df8db
narramadan <narra.madan@outlook.com>
2018-06-26 15:11:11 +1000

    Added support for global build and individual project build

--- api/src/main/java/com/mail/publisher/WebConfig.java
+++ api/src/main/java/com/mail/publisher/WebConfig.java
@@ -2,13 +2,12 @@ package com.mail.publisher;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
- 
+
+	// Support CORS to be integrate APIs from external UI
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
